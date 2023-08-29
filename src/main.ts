fa9144c20395fca032e19e34f33f58009fcf43c5
Rafael-RD <rafaelreinertd@hotmail.com>
2023-08-29 00:42:02 -0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, new DocumentBuilder().build()));
+
+  await app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}. Docs: http://localhost:${process.env.PORT}/api`));
 }
 bootstrap();
